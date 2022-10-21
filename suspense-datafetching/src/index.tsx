165b42c4011c72d2f0d9e2606a426d25e2ce686e
Ryosuke Fujii <ryosuke.fujii@mazrica.com>
2022-10-22 00:37:29 +0900

    add index.html

--- suspense-datafetching/src/index.tsx
+++ suspense-datafetching/src/index.tsx
@@ -1,6 +1,5 @@
 import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 // import App from './App';
 import {DataLoader} from './DataLoader'
 import reportWebVitals from './reportWebVitals';
